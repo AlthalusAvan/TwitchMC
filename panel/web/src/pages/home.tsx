90d2c8e6f4af6f5ee0904fe543b7f8bb92302709
MacDara Roche <mroche7@tcd.ie>
2022-04-18 17:11:28 +0100

    Remove workflows, add info

--- panel/web/src/pages/home.tsx
+++ panel/web/src/pages/home.tsx
@@ -1,22 +1,44 @@
 import React from "react";
 
-import { Heading, Button, Link, Flex } from "@chakra-ui/react";
+import { Heading, Button, Link, Flex, Text, Container } from "@chakra-ui/react";
 import { Link as BrowserLink } from "react-router-dom";
 import Layout from "../components/layout";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 export default function Home() {
   console.log(process.env.NODE_ENV);
 
   return (
     <Layout title="Home">
-      <Flex direction="column" alignItems="center" justify="center" gap="1.5em">
-        <Heading as="h1" size="lg">
-          Hello World
-        </Heading>
-        <Link as={BrowserLink} to="/login">
-          <Button>Login with Twitch</Button>
-        </Link>
-      </Flex>
+      <Container maxW="2xl">
+        <Flex
+          direction="column"
+          alignItems="center"
+          justify="center"
+          gap="1.5em"
+        >
+          <Heading as="h1" size="lg">
+            Welcome to TwitchMC
+          </Heading>
+          <Text>
+            This project is still in develpoment - if you notice any issues,
+            please let the moderators of the server you play on know! More
+            information will be added to this page over time, but for now you
+            can learn more on{" "}
+            <Link
+              color="purple.500"
+              href="https://github.com/AlthalusAvan/TwitchMC"
+              target="_blank"
+            >
+              Github <ExternalLinkIcon fontSize=".8em" />
+            </Link>
+            .
+          </Text>
+          <Link as={BrowserLink} to="/login">
+            <Button>Login with Twitch</Button>
+          </Link>
+        </Flex>
+      </Container>
     </Layout>
   );
 }
